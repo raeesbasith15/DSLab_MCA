8307ff45817d4bc46faa1506f06d80d1ed76b453
Raees Basith <raeesbasith@gmail.com>
2026-09-16 20:53:23 +0530

    Complete stack using array program

--- stack_array.c
+++ stack_array.c
@@ -26,17 +26,52 @@ void pop() {
     top--;
     printf("\nElement removed is : %d", val);
 }
+
+void peek() {
+    if (top == -1) {
+        printf("\n-Stack underflow!-");
+        return;
+    }
+    printf("\nTop of the stack is : %d", stack[top]);
+}
 void display() {
     if (top == -1) {
         printf("\nStack empty!!!");
         return;
     }
     printf("\n--Stack--\n");
-    for (int i = top - 1; i >= 0; i++) {
+    for (int i = top; i >= 0; i--) {
         printf("%d\n", stack[i]);
     }
 }
 
 int main() {
-    
+    int ch;
+    while(1) {
+        printf("\n---- Stack Operations ----\n");
+        printf("\n-Operations-\n");
+        printf("1. Push\n2. Pop\n3. Peek\n4. Display\n5. Exit\n");
+        printf("\nSelect an operation : ");
+        scanf("%d",&ch); 
+
+    switch(ch) {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            printf("Exiting...");
+            return 0;
+        default:
+            printf("Invalid choice!!!");
+    }
+    }
 }
